8b331adf5d0aaf430b4959cfd80f637b768d3809
mdnriyas17 <mdnriyas@gmail.com>
2024-08-22 11:04:12 +0530

    work data change

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -290,7 +290,7 @@ const Work = () => {
         <div className="aspect-square bg-[url('./assets/work-4.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
           <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7" onClick={() => {
             Modal.info({
-              title: "Frontend project",
+              title: "Full Stack project",
               content: (
                 <>
                   <div>
@@ -344,7 +344,7 @@ const Work = () => {
         <div className="aspect-square bg-[url('./assets/work-4.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
           <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7" onClick={() => {
             Modal.info({
-              title: "Frontend project",
+              title: "Full Stack project",
               content: (
                 <>
                   <div>
