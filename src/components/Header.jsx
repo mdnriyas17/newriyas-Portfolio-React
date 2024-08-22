f2cb1946a450742585bd7c19a8a6861a05fee79e
mdnriyas17 <mdnriyas@gmail.com>
2024-08-22 14:24:31 +0530

    scize

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,19 +6,19 @@ import { TypeAnimation } from 'react-type-animation';
 
 const Header = () => {
   return (
-    <div className="w-full text-center h-screen flex flex-col items-center justify-center gap-4 mt-8 sm:mt-8">
+    <div className="w-full text-center h-screen flex flex-col items-center justify-center gap-4 mt-8 sm:mt-4">
       <img src={profile_img} alt="" className="rounded-full w-32" />
       <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
         Hi! I'm Mohamedriyas P</h3>
     <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">
-      {/* <TypeAnimation 
+      <TypeAnimation 
       sequence={["Full Stack Web Developer",1000, "Frontend Developer",1000, "MERN Stack Developer",1000]}
       wrapper="span"
       repeat={Infinity}
       speed={2}
 
-      /> */}
-      Full Stack Web Developer
+      />
+      {/* Full Stack Web Developer */}
     </h1>
     <p className="max-w-2xl mx-auto font-Ovo">I am a Full Stack Web Developer with over a decade of professional expertise in the field. Throughout my career, I have had the privilege of collaborating with Prest organizations, contributing to their success and growth.</p>
 
