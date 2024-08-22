b306a6eac04dfccb03ab5733b5a04a41aadb5781
mdnriyas17 <mdnriyas@gmail.com>
2024-08-22 13:08:29 +0530

    woek

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -18,7 +18,7 @@ const Work = () => {
       <div data-aos="fade-up-left">
       <h4 className="text-center mb-2 text-lg font-Ovo">My portfolio</h4>
       <h2 className="text-center text-5xl font-Ovo">My latest work</h2>
-      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+      <p className="text-center max-w-2xl  mt-5 mb-12 font-Ovo">
         Welcome to my web development portfolio! Explore a collection of
         projects showcasing my expertise in Full Stack web development.
       </p>
